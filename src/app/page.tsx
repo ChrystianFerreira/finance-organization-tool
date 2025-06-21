7bd3afe263202dbc1f25fc7d5bd62f11fbd01969
Chrystian Ferreira <chrystian.profissional@gmail.com>
2025-06-21 18:03:46 -0300

    feat: init project

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import FinancialOrganizer from './big-racks-generator/page';
+
+/**
+ * The main page component that renders the HomePage component.
+ *
+ * @returns {JSX.Element} The rendered HomePage component.
+ */
+const Page = () => {
+    return <p>TODO</p>;
+};
+
+export default Page;
